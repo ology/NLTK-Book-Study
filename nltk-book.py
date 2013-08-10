f98061227140d562fc9939fdc128db236b50b5a5
Gene Boggs <gene.boggs@gmail.com>
2013-08-10 15:29:36 -0700

    initial import

---
+++ nltk-book.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+# -- coding: utf-8 --
+#
+# Work through the http://nltk.org/book/
+# -- gene + github at ology dot net not dot com
+#
+# Handy links: http://nltk.org/
+# http://stackoverflow.com/questions/4092994/unable-to-install-matplotlib-on-mac-os-x
+#
+# Use floating point division.
+from __future__ import division
+from nltk.book import *
+
