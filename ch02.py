c87e9a954c96a66b4c9073c83b3e1584a4ec8771
Gene Boggs <gene.boggs@gmail.com>
2013-08-14 15:35:46 -0700

    Made the udhr CFD a sub

--- ch02.py
+++ ch02.py
@@ -98,9 +98,10 @@ from nltk.corpus import udhr
 # TODO How to list all languages?
 languages = ['Chickasaw', 'English', 'German_Deutsch', 'Greenlandic_Inuktikut',
     'Hungarian_Magyar', 'Ibibio_Efik']
-cfd = nltk.ConditionalFreqDist(
-    (lang, len(word))
-    for lang in languages
-    for word in udhr.words(lang + '-Latin1'))
-cfd.plot(cumulative=True)
+def udhr_cond_freq_dist(udhr, languages):
+    cfd = nltk.ConditionalFreqDist(
+        (lang, len(word))
+        for lang in languages
+        for word in udhr.words(lang + '-Latin1'))
+    cfd.plot(cumulative=True)
 
