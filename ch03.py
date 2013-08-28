99ad0753486a0877a2b9e496617c4e2459281c65
Gene Boggs <gene.boggs@gmail.com>
2013-08-28 12:40:15 -0700

    Started ch3 with a handy inspect() function

--- ch03.py
+++ ch03.py
@@ -8,3 +8,22 @@ import nltk, re, pprint
 # -- gene + github at ology dot net not dot com
 # ------------------------------------------------------------------------------
 
+def inspect(it):
+    print 'type=%s, len=%d' % (type(it).__name__, len(it)),
+    if isinstance(it, str):
+        print 'chars'
+    elif isinstance(it, list):
+        print 'elements'
+
+from urllib import urlopen
+url = "http://www.gutenberg.org/files/2554/2554.txt"
+raw = urlopen(url).read()
+# Or this:
+#proxies = {'http': 'http://www.someproxy.com:3128'}
+#raw = urlopen(url, proxies=proxies).read()
+inspect(raw) #type=str, len=1176893 chars
+raw[31:73] #'Crime and Punishment, by Fyodor Dostoevsky'
+
+tokens = nltk.word_tokenize(raw)
+inspect(tokens) #type=list, len=244484 elements
+tokens[5:12] #['Crime', 'and', 'Punishment', ',', 'by', 'Fyodor', 'Dostoevsky']
