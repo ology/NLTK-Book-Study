9b9a5c361a9b605df6e045d133352e801aa0fb50
Gene Boggs <gene.boggs@gmail.com>
2013-08-28 11:02:45 -0700

    Setup ch3 study notes

---
+++ ch03.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+# -- coding: utf-8 --
+from __future__ import division
+import nltk, re, pprint
+
+# ------------------------------------------------------------------------------
+# Work through http://nltk.org/book/ch03.html
+# -- gene + github at ology dot net not dot com
+# ------------------------------------------------------------------------------
+
