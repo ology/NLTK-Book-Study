846452076315bbd579323239969094f2acaf86c1
Gene Boggs <gene.boggs@gmail.com>
2013-08-28 13:13:26 -0700

    Handled raw HTML

--- ch03.py
+++ ch03.py
@@ -34,3 +34,29 @@ text = nltk.Text(tokens)
 inspect(text) #type=Text, len=244484 nltk tokens
 text[982:1019] #['CHAPTER', 'I', 'On', 'an', 'exceptionally', 'hot', 'evening', 'early', 'in', 'July', 'a', 'young', 'man', 'came', 'out', 'of', 'the', 'garret', 'in', 'which', 'he', 'lodged', 'in', 'S.', 'Place', 'and', 'walked', 'slowly', ',', 'as', 'though', 'in', 'hesitation', ',', 'towards', 'K.', 'bridge.']
 
+#
+text.collocations()
+
+# Find the bounds of the novel.
+raw.find("PART I") #5338
+raw.find("CHAPTER I") #5352
+raw.rfind("End of Project Gutenberg") #1157743
+# Slice out the novel content.
+raw = raw[raw.find('PART I'):raw.rfind('End of Project Gutenberg')]
+inspect(raw) #type=str, len=1152405 chars
+
+# Download a web page.
+url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
+html = urlopen(url).read()
+html[:60]
+# Purge markup.
+raw = nltk.clean_html(html)
+tokens = nltk.word_tokenize(raw)
+# Find content bounds.
+tokens[120:391] #['Scientists', 'believe' ... 'wo', "n't", 'disappear.']
+# Reset the tokens to the found content.
+tokens = tokens[120:391]
+text = nltk.Text(tokens)
+inspect(text) #type=Text, len=271 nltk tokens
+text.concordance('gene') #Displaying 4 of 4 matches: ...
+
